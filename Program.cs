548c4408c3e105427c2b332e0d37c429c910a047
MightyFixie <maximusf625@gmail.com>
2020-09-10 16:26:29 +0300

    second commit

--- Program.cs
+++ Program.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GitLab
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace GitLab {
+    class Program {
+        static void Main(string[] args) {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Done!");
+            Console.ReadKey();
         }
     }
 }
